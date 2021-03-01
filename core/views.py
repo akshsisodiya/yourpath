89e29af7aad30d4f5047997e5f5d33f1e01046f7
aksh <sisodiyaaksh@gmail.com>
2021-03-01 13:41:32 +0530

    basic files created

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'core/home.html')
+
+def about_us(request):
+    return render(request, 'core/about_us.html')
+
+def contact_us(request):
+    return render(request, 'core/contact_us.html')
