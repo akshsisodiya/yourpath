9cedacc7ce230a00d73fb44f23ab25e7be7b02e8
aksh <sisodiyaaksh@gmail.com>
2021-05-08 12:56:55 +0530

    'home-page-changed'

--- yourpath/settings.py
+++ yourpath/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'uegeguxg)!p7=fio**6a_c9j&a6q*&o_5syhkqk#g(v7rs5$j!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR =[
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
