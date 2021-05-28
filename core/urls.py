188cb06b4ea6f52e42e2abefe114c33edb9eab2c
Aman <47911791+aman162000@users.noreply.github.com>
2021-05-28 17:44:26 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,8 @@ from .views import *
 from django.contrib import admin
 urlpatterns = [
     path('',index),
-    path('/profiles',index),
-    path('/upload',index),
+    path('profile',index),
+    path('upload',index),
     path('add-like/<int:id>/', addlike),
     path('follow/<str:username>/', addFollower),
     path('save-post/<int:id>/', savePost),
