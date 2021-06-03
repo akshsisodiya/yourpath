b6e70affd087851a89f418bf6daf30650c4c5571
aman162000 <borse.aman@rediffmail.com>
2021-06-03 18:55:17 +0530

    Files Updated

--- core/views.py
+++ core/views.py
@@ -132,7 +132,7 @@ def addLikeToComment(request,id):
     return HttpResponse(r, "application/json")
 
 #@login_required(login_url='/auth/login/')
-@require_http_methods(["POST"])
+@require_http_methods(["GET"])
 def deletePost(request,id):
     msg = ''
     try:
